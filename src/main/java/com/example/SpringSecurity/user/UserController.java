ee01984db6d741df3264730e0b834e63288b2f40
Kacper <skikac23@wp.pl>
2023-09-12 20:48:55 +0200

    Main page controller, thymeleaf, changed security of some endpoints

--- src/main/java/com/example/SpringSecurity/user/UserController.java
+++ src/main/java/com/example/SpringSecurity/user/UserController.java
@@ -1,21 +1,21 @@
 package com.example.SpringSecurity.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
+    @GetMapping("/users")
     public List<User> getUsers(){
         return userService.getUsers();
     }
+
 }
